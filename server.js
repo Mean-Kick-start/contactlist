ec02128ca0e44ed6ca309f131c1a1e33390181ce
shantanu26 <shantanu_singh83@yahoo.com>
2016-02-22 21:29:54 -0700

    I have made changes

--- server.js
+++ server.js
@@ -3,38 +3,38 @@ var app = express();
 var mongojs=require('mongojs');
 var bodyparser=require('body-parser');
 app.use(bodyparser.json());
-var db = mongojs('contactlist',['contactlist']);
+var db = mongojs('contactList',['contactList']);
 app.get('/contactlist', function(req, res){
     console.log('Hello listening to the get request');
-   db.contactlist.find(function(err, docs){
+   db.contactList.find(function(err, docs){
        console.log(docs);
        res.json(docs);
    })
 });
 app.post('/contactlist' , function(req, res){
     console.log(req.body);
-    db.contactlist.insert(req.body, function(err, docs){
+    db.contactList.insert(req.body, function(err, docs){
         res.json(docs);
     })
 });
 app.delete('/contactlist/:id' , function(req, res){
     var id= req.params.id;
-    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err,docs){
+    db.contactList.remove({_id: mongojs.ObjectId(id)}, function(err,docs){
         res.json(docs);
     })
 });
 app.get('/contactlist/:id' , function(req, res){
    var id = req.params.id;
     console.log(id);
-    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, docs){
+    db.contactList.findOne({_id: mongojs.ObjectId(id)}, function(err, docs){
         res.json(docs);
     })
 });
 app.put('/contactlist/:id' , function(req, res){
   var id = req.params.id;
     console.log(req.body.name);
-    db.contactlist.findAndModify({query:{_id: mongojs.ObjectId(id)},
-        update: {$set: {name:req.body.name, number: req.body.number, gender:req.body.gender}},
+    db.contactList.findAndModify({query:{_id: mongojs.ObjectId(id)},
+        update: {$set: {name:req.body.name, email: req.body.email, number:req.body.number}},
         new: true},function(err,docs){
         res.json(docs);
     })
